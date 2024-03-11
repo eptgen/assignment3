03dd04fee3a451c36a26a21511cfe4d19e0fee9d
23jsr7 <evolene@cs.williams.edu>
2024-03-10 21:06:58 -0400

    debug

--- renderer.py
+++ renderer.py
@@ -41,7 +41,7 @@ class VolumeRenderer(torch.nn.Module):
         rays_feature: torch.Tensor
     ):
         print("weights/rays shape", weights.shape, rays_feature.shape)
-        return torch.sum(weights * rays_feature, dim = -1)
+        return torch.sum(weights.unsqueeze(-1) * rays_feature.reshape(weights.shape[0], weights.shape[1], -1), dim = 1)
 
     def forward(
         self,
