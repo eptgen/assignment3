52803c8138621fe73b016a0ef56ae130aa142ed2
23jsr7 <evolene@cs.williams.edu>
2024-03-11 22:03:59 -0400

    debug

--- renderer.py
+++ renderer.py
@@ -70,7 +70,7 @@ class VolumeRenderer(torch.nn.Module):
             density = implicit_output['density']
             feature = implicit_output['feature']
             print("density", density.shape)
-            print("density sum", torch.sum(density))
+            print("density L0", torch.norm(density, p = 0))
 
             # Compute length of each ray segment
             depth_values = cur_ray_bundle.sample_lengths[..., 0]
