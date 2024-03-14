707d7b842eba7a9cb95ccc64811ae8f0c08f0140
23jsr7 <evolene@cs.williams.edu>
2024-03-14 00:54:07 -0400

    surface

--- surface_rendering_main.py
+++ surface_rendering_main.py
@@ -237,8 +237,8 @@ def train_points(
 
             # Get distances and enforce point cloud loss
             distances, gradients = model.implicit_fn.get_distance_and_gradient(points)
-            loss = None # TODO (Q6): Point cloud SDF loss on distances
-            point_loss = loss
+            
+            loss = torch.norm(distances)
 
             # Sample random points in bounding box
             eikonal_points = get_random_points(
