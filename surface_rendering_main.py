ffa3d3f39b56f90bcd774956d30f37bba3eed0cf
23jsr7 <evolene@cs.williams.edu>
2024-03-15 02:15:25 -0400

    color

--- surface_rendering_main.py
+++ surface_rendering_main.py
@@ -367,6 +367,7 @@ def train_images(
             out = model(ray_bundle)
 
             # Color loss
+            print(out)
             loss = torch.mean(torch.square(rgb_gt - out['color']))
             image_loss = loss
 
